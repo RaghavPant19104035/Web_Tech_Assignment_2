786264c2f22f65339e6aebda72ec437a25fa6867
RaghavPant19104035 <113606558+RaghavPant19104035@users.noreply.github.com>
2022-09-25 23:14:06 +0530

    first commit

--- Question_2.java
+++ Question_2.java
@@ -21,7 +21,7 @@ public class Question_2 {
 		else if(b < a && b < c) { // // if b is less than a and c then a would be minimum
 			System.out.println("Smallest ans is " + b);
 		}
-		else { // c would be minimum if both the above statements are not true
+		else { // c would be minimum if both the above statements are untrue
 			System.out.println("Smallest ans is " + c);
 		}
 		
